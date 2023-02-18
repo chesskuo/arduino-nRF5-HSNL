434b0d34e674b44c82fd589bc1687fae43fa8b2d
Chess Kuo <owo@chesskuo.tw>
2023-02-18 23:05:20 +0800

    fix syntax

--- variants/Generic/variant.h
+++ variants/Generic/variant.h
@@ -43,13 +43,13 @@ extern "C"
 
 // LEDs
 //#define PIN_LED              (13)
-//#define LED_BUILTIN          PIN_LED 
+//#define LED_BUILTIN          PIN_LED
 
 /*
  * Analog pins
  */
-#define PIN_A0               (31)	//p0.31 for Battery/2, for GAT4485
-//#define PIN_A1               (29)	//this setting is for weatherBit
+#define PIN_A0               (31)  //p0.31 for Battery/2, for GAT4485
+//#define PIN_A1               (29)  //this setting is for weatherBit
 //#define PIN_A2               (3)
 //#define PIN_A3               (4)
 //#define PIN_A4               (5)
@@ -71,62 +71,54 @@ static const uint8_t A0  = PIN_A0 ;
  * Serial interfaces
  */
 // Serial
-//for 4553 V1, 4485 V1.X, 4038, 水閥控制器, 2485 V1.0, V1.2, V1.3; 4485 V1.2, V1.3, 2485 V1.0(20210410 and later)
-//#define PIN_SERIAL_RX       (6)	
-//#define PIN_SERIAL_TX       (8)		
+// for 4553 V1, 4485 V1.X, 4038, 水閥控制器, 2485 V1.0, V1.2, V1.3; 4485 V1.2, V1.3, 2485 V1.0(20210410 and later)
+//#define PIN_SERIAL_RX       (6)
+//#define PIN_SERIAL_TX       (8)
 
-//for DK52, 2553, ---  
-//4553 V2.0~V2.2, 4038 V2, 4485 V2, 2038 V2.0~V2.1, 52840
-//SIN-020-V1.0, SIN-021
-#define PIN_SERIAL_RX       (8)		
-#define PIN_SERIAL_TX       (6)		
+// for DK52, 2553, ---
+// 4553 V2.0~V2.2, 4038 V2, 4485 V2, 2038 V2.0~V2.1, 52840
+// SIN-020-V1.0, SIN-021
+#define PIN_SERIAL_RX       (8)
+#define PIN_SERIAL_TX       (6)
 
 /*
  * SPI Interfaces, for NB4485 or LORA2485
  */
- 
 #define SPI_INTERFACES_COUNT 1
+
 #define PIN_SPI_MISO         (5)
 #define PIN_SPI_MOSI         (3)
 #define PIN_SPI_SCK          (4)
 
 static const uint8_t SS   = 2 ;
-/*
-#define PIN_SPI_MISO         (22)
-#define PIN_SPI_MOSI         (23)
-#define PIN_SPI_SCK          (24)
-
-static const uint8_t SS   = 25 ;
-*/
 static const uint8_t MOSI = PIN_SPI_MOSI ;
 static const uint8_t MISO = PIN_SPI_MISO ;
 static const uint8_t SCK  = PIN_SPI_SCK ;
-
 
 /*
  * Wire Interfaces
  */
 #define WIRE_INTERFACES_COUNT 1
 
-//for RTC, 2038 V2.1, 
-//#define PIN_WIRE_SDA         (12u)	//(19u)
-//#define PIN_WIRE_SCL         (11u)	//(18u)
+// for RTC, 2038 V2.1
+//#define PIN_WIRE_SDA         (12u)  //(19u)
+//#define PIN_WIRE_SCL         (11u)  //(18u)
 
-//for GAT product LIS3DH, 2485, 4485 model, 2038
+// for GAT product LIS3DH, 2485, 4485 model, 2038
 //#define PIN_WIRE_SDA         (19u)
 //#define PIN_WIRE_SCL         (18u)
 
-//next two for 4553 -- 4553 V2.0~2.2
+// next two for 4553 -- 4553 V2.0~2.2
 #define PIN_WIRE_SDA         (12u)
 #define PIN_WIRE_SCL         (11u)
 
-//next two for 52840
-//#define PIN_WIRE_SDA         (27)	//(26u)
-//#define PIN_WIRE_SCL         (26)	//(27u)
-//#define PIN_WIRE_SDA         (26u)	//(26u)
-//#define PIN_WIRE_SCL         (27u)	//(27u)
+// next two for 52840
+//#define PIN_WIRE_SDA         (27)  //(26u)
+//#define PIN_WIRE_SCL         (26)  //(27u)
+//#define PIN_WIRE_SDA         (26u)  //(26u)
+//#define PIN_WIRE_SCL         (27u)  //(27u)
 
-//next don't know for which model
+// next don't know for which model
 //#define PIN_WIRE_SDA         (20u)
 //#define PIN_WIRE_SCL         (17u)
 
@@ -136,10 +128,10 @@ static const uint8_t SCL = PIN_WIRE_SCL;
 /*
  * Reset Button at P0.21
  */
-#define RESET_PIN            21	
+#define RESET_PIN            (21)
 
-//next for 52840
-//#define RESET_PIN            18	
+// next for 52840
+//#define RESET_PIN            (18)
 
 #ifdef __cplusplus
 }
